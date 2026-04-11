d4bee2726e626cdf85b9df33a63174ba0e7944cc
Arif Muhasyir <arthurash911@gmail.com>
2026-04-11 14:54:39 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
-/**
- * Entry point untuk menjalankan Panorama Cek Bio Bot
- * Cukup import bot agar auto-run saat di-execute
- */
+import "dotenv/config";
 import "./telegram/panorama-bot";
 
 console.log("🚀 Panorama Bot starting...");
